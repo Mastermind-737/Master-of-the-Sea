6aba5773c27efdcfb366ee011f51967f6f2d7f14
Antonio Fontan <35477462+Mastermind-737@users.noreply.github.com>
2018-04-30 11:57:19 -0700

    Update map_engine.py

--- MyFinalProject/code/map_engine.py
+++ MyFinalProject/code/map_engine.py
@@ -1,62 +1,47 @@
 import pygame
-from main.textures import *
-from main.Globals import *
+from textures import*
 
-class MapEngine():
+class Map_Engine:
 
-
-    @staticmethod
-    def dec_map(map_data):
-        dimensones = (0,0)
-        ty = 0
-        map_datad = []
-        for i in map_data:
-            i = i.replace("\n","")
-            i = i.replace(" ", "")
-            map_datax = i.split(";")
-            tx = 0
-            for i in map_datax:
-                xdata = i.split(",")
-                if not xdata[1].endswith("$"):
-                    map_datad.append((tx + 1,ty,xdata[0], int(xdata[1])))
-                    tx += int(xdata[1])
-                else:
-                    xdata[1] = xdata[1].replace("$","")
-                    dimensones = [int(xdata[0]),int(xdata[1])]
-            ty += 1
-
-        decoded_map = []
-        for i in map_datad:
-            for x in range(int(i[3])):
-                    decoded_map.append((i[0] + x, i[1], i[2]))
-
-        return decoded_map , dimensones
-
-    @staticmethod
-    def add_tile(Texture,x,y,Terrain):
-        Terrain.blit(Texture,(x * Tiles.Size ,y * Tiles.Size))
+    def add_tile(tile, pos, addTo):
+        addTo.blit(tile, (pos[0] * Tiles.size, pos[1] * Tiles.size))
 
 
-    @staticmethod
     def load_map(file):
-        mapdata = open(file,mode="r").readlines()
-        map_data = MapEngine.dec_map(mapdata)
+        with open(file, "r") as mapfile:
+            map_data = mapfile.read()
 
-        map_size = [map_data[1]]
-        map_data = list(map_data)[:-1]
+        # Read Map Data
+        map_data = map_data.split("-")   # Split into list of tiles
 
-        map_size[0][0] *= Tiles.Size
-        map_size[0][1] *= Tiles.Size
+        map_size = map_data[len(map_data) - 1]   # Get map dimensions
+        map_data.remove(map_size)
+        map_size = map_size.split(",")
+        map_size[0] = int(map_size[0]) * Tiles.size
+        map_size[1] = int(map_size[1]) * Tiles.size
+
+        tiles = []
+
+        for tile in range(len(map_data)):
+            map_data[tile] = map_data[tile].replace("\n", "")
+            tiles.append(map_data[tile].split(":"))   # Split pos from texture
+
+        for tile in tiles:
+            tile[0] = tile[0].split(",")   # Split pos into list
+            pos = tile[0]
+            for p in pos:
+                pos[pos.index(p)] = int(p)   # Convert to integer
+
+            tiles[tiles.index(tile)] = (pos, tile[1])   # Save to tile list
 
 
-        #gen Terrain
+        # Create Terrain
+        terrain = pygame.Surface(map_size, pygame.HWSURFACE)
 
-        Terrain = pygame.Surface(map_size[0],pygame.HWSURFACE|pygame.SRCALPHA)
+        for tile in tiles:
+            if tile[1] in Tiles.Texture_Tags:
+                Map_Engine.add_tile(Tiles.Texture_Tags[tile[1]], tile[0], terrain)
 
-        for tile in map_data[0]:
-            if tile[2] in Tiles.Texture_Tags:
-                MapEngine.add_tile(Tiles.Texture_Tags[tile[2]],tile[0],tile[1],Terrain)
-            else:
-                print("Tile : "  + str(tile[2]) + "is not a Valid Tile type !")
 
-        return Terrain
+
+        return terrain
